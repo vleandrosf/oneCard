c70ffb16e2d2a769e9be59020dbd4734ba63ee0d
Virginia Leandro <vleandro@salesforce.com>
2021-04-27 14:59:48 -0500

    First commit

---
+++ cardOne.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+import CARDONE_IMAGE from '@salesforce/resourceUrl/cardone';
+
+
+export default class HomeImage extends LightningElement {
+    cardOne = CARDONE_IMAGE;
+}
